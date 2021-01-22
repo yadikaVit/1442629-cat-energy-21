a06b39e7477e2fe0703146dfca0a3979464563dc
yadikaVit <yadika@mail.ru>
2021-01-22 17:51:56 +0300

    ПП

--- source/js/script.js
+++ source/js/script.js
@@ -12,3 +12,28 @@ navToggle.addEventListener('click', function() {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+
+//cats
+
+var buttonBefore = document.querySelector(".example__range-button--before");
+var imageBefore = document.querySelector(".example__image--before");
+var buttonAfter = document.querySelector(".example__range-button--after");
+var imageAfter = document.querySelector(".example__image--after");
+var barBefore = document.querySelector(".example__bar");
+
+buttonBefore.addEventListener('click', function() {
+  imageBefore.classList.remove("example_modal-close");
+  imageBefore.classList.add("example_modal-show");
+  imageAfter.classList.add("example_modal-close");
+  barBefore.classList.remove("example__bar-after");
+  barBefore.classList.add("example__bar");
+});
+
+buttonAfter.addEventListener("click", function (evt) {
+  imageAfter.classList.remove("example_modal-close");
+  imageAfter.classList.add("example_modal-show");
+  imageBefore.classList.add("example_modal-close");
+  barBefore.classList.remove("example__bar");
+  barBefore.classList.add("example__bar-after");
+});
